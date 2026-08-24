e329ee93c1f410e5efbe99871247a2ff16a2b92d
Triv24 <triveni01gauri@gmail.com>
2026-08-24 21:55:57 +0530

    Added the CORS origin for the deployed frontend

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:8000", "http://localhost:5173", "http://localhost:5175"],
+    allow_origins=["http://127.0.0.1:8000", "http://localhost:5173", "http://localhost:5175", "https://intervue-frontend-io.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
